2539aa69bb47f34e757298d8bbd77f3dd0e370da
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-02-11 23:11:32 +0200

    src js-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+// поиск элемента
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+btnStop.disabled = true; // начальное состояние кнопки: выключена
+let colorInterval = null; // таймер для смены цвета
+
+// функция генерации случайного цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// додаем слушателя для кнопки СТАРТ
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true; // деактивация кнопки СТАРТ
+  btnStop.disabled = false; // активация кнопки СТОП
+
+  // таймер для смены цвкта
+  colorInterval = setInterval(() => {
+
+    // покраска фона в случайный цвет
+    document.body.style.background = getRandomHexColor();
+  }, 1000); // смена цвета раз в секунду
+});
+
+// додаем слушателя кнопки СТОП
+btnStop.addEventListener('click', () => {
+
+  // очистка таймера смены цвета
+  clearInterval(colorInterval);
+
+  btnStart.disabled = false; // активация кнопки СТАРТ
+  btnStop.disabled = true; // деактивация кнопки СТОП
+
+});
